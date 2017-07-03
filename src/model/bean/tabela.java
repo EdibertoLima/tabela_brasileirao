623faacfca9e8a14ca0c47c23493adac16ec56ce
ediberto <ediberto45@gmail.com>
2017-07-03 10:40:47 -0300

    v1

---
+++ src/model/bean/tabela.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.bean;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author betim
+ */
+
+public class tabela {
+    
+    int b_time[][] = new int[4][2];
+    int  r_Pontos[][]=new int[4][8];
+    String nomes_time[]= new String[4];
+    String ano;
+    
+   
+    public void resultados(int c,int f,int i) {
+      
+                b_time[i][0]=c;
+                b_time[i][1]=f;
+              
+                            
+    }
+    public void setnomes(String s,int i) {
+      
+                nomes_time[i]=s;    
+                
+    }
+    public String getnomes(int i) { 
+              
+                return nomes_time[i];    
+       }           
+    
+    
+    public int getTabela(int x , int j){
+    
+            return r_Pontos[x][j] ;
+ 
+    }
+    public void setTabela(int k , int o,int s){
+            
+             r_Pontos[k][o]= s;
+ 
+    }
+    public String getano(){
+    
+        return ano;
+    }
+    
+    public void setano(String a){
+    
+    ano= a;
+    
+    
+    }
+    
+    
+    public void setPontos(){
+        for (int i = 0; i < 4; i++) {
+          if(b_time[i][0]>b_time[i][1]) { 
+          r_Pontos[i][0]=3;  }
+            
+          if(b_time[i][0]==b_time[i][1]){  
+          r_Pontos[i][0]=1;  }
+          if(b_time[i][0]==b_time[i][1]){  
+          r_Pontos[i][0]=1;  }
+    }  
+    }
+    
+   
+    public void setVitorias(){
+        for (int i = 0; i < 4; i++) {
+          if(b_time[i][0]>b_time[i][1]){ 
+          r_Pontos[i][2]=1;  
+        }else r_Pontos[i][2]=0;      
+    }}
+    public void setDerrotas(){
+        for (int i = 0; i < 4; i++) {
+          if(b_time[i][0]<b_time[i][1])  
+          r_Pontos[i][3]=1;  
+        }      
+    }
+    public void setEmpate(){
+        for (int i = 0; i < 4; i++) {
+          if(b_time[i][0]== b_time[i][1])  
+          r_Pontos[i][4]=1;  
+        }      
+    }
+    
+    public void setGols_feitos(){
+    for (int i = 0; i < 4; i++) {      
+          r_Pontos[i][5]=b_time[i][0];  
+        }   
+    }
+    public void setGols_sofridos(){
+    for (int i = 0; i < 4; i++) {      
+          r_Pontos[i][6]=b_time[i][1];  
+        }   
+    }
+    public void setS_gols(){
+    for (int i = 0; i < 4; i++) {      
+          r_Pontos[i][7]=b_time[i][0]-b_time[i][1];  
+        }   
+    }
+    
+     public void setTabela(){
+        tabela t = new tabela();
+        t.setVitorias();
+        t.setDerrotas();
+        t.setEmpate();
+        t.setGols_feitos();
+        t.setGols_sofridos();
+        t.setS_gols();
+  
+}
+
+    
+     
+}
+
+
---
+++ src/model/dao/TabelaDao.java
@@ -0,0 +1,218 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.dao;
+
+import com.sun.javafx.scene.control.skin.VirtualFlow;
+import connection.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import model.bean.tabela;
+
+/**
+ *
+ * @author betim
+ */
+public class TabelaDao {
+    public int campeonatoid=0;
+    
+    /**
+     *
+     * @param t
+     */
+    public void create(tabela t){
+        
+        Connection con = ConnectionFactory.getConnection();
+       
+        PreparedStatement stmt = null;
+        try {
+            stmt = con.prepareStatement("UPDATE tabela set pontos = pontos + ? ,vitorias= vitorias + ? ,derrotas= derrotas + ?,empates= empates + ?"
+                    + " ,gols_feitos = gols_feitos + ?, gols_sofridos = gols_sofridos + ?, saldo_gols = saldo_gols + ?, jogos = jogos +1, campeonatoid = ? WHERE time=?");
+            for (int i = 0; i < 4; i++) {
+
+            stmt.setInt(1, t.getTabela(i, 0));
+            stmt.setInt(2, t.getTabela(i, 2));
+            stmt.setInt(3, t.getTabela(i, 3));
+            stmt.setInt(4, t.getTabela(i, 4));
+            stmt.setInt(5, t.getTabela(i, 5));
+            stmt.setInt(6, t.getTabela(i, 6));
+            stmt.setInt(7, t.getTabela(i, 7));
+            stmt.setInt(8,campeonatoid);
+            stmt.setString(9, t.getnomes(i));
+            stmt.executeUpdate();
+            }
+   
+            
+            JOptionPane.showMessageDialog(null, "Salvo com sucesso");
+        } catch (SQLException ex) {
+            Logger.getLogger(TabelaDao.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+            
+         ConnectionFactory.closeConnection(con, stmt);
+        
+        }    
+    }
+    
+    
+    public List<tabela> read(){
+    
+     Connection con = ConnectionFactory.getConnection();
+     
+     PreparedStatement stmt = null;
+     ResultSet rs = null;
+     
+    
+        List<tabela> tabelas = new ArrayList<>();
+     
+        try {
+            stmt = con.prepareStatement("SELECT * FROM `tabela` WHERE 1 ORDER BY pontos DESC ");
+            rs= stmt.executeQuery();
+         
+            while (rs.next()) {                
+                
+              tabela t = new tabela();
+ 
+              t.setnomes(rs.getString("time"),0);
+              t.setTabela(0, 0, rs.getInt("pontos"));
+              t.setTabela(0, 1, rs.getInt("jogos"));
+              t.setTabela(0, 2, rs.getInt("vitorias"));
+              t.setTabela(0, 3, rs.getInt("derrotas"));
+              t.setTabela(0, 4, rs.getInt("empates"));
+              t.setTabela(0, 5, rs.getInt("gols_feitos"));
+              t.setTabela(0, 6, rs.getInt("gols_sofridos"));
+              t.setTabela(0, 7, rs.getInt("saldo_gols"));          
+              tabelas.add(t);
+            }  
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(TabelaDao.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+        
+        ConnectionFactory.closeConnection(con, stmt, rs);
+        
+        }
+              
+        return tabelas;
+
+    }
+     
+public void removedados(){
+        Connection con = ConnectionFactory.getConnection();
+       
+        PreparedStatement stmt = null;
+        try {
+            stmt = con.prepareStatement("UPDATE tabela set pontos=0,jogos=0,vitorias=0,derrotas=0,empates=0,gols_feitos=0,gols_sofridos=0 ,saldo_gols=0 WHERE 1");
+            stmt.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Dados removidos com sucesso");
+        } catch (SQLException ex) {
+            Logger.getLogger(TabelaDao.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+            
+         ConnectionFactory.closeConnection(con, stmt);
+        
+        }    
+    }
+    
+public List<tabela> pesquisaTabelaPorAno(){
+        Connection con = ConnectionFactory.getConnection();
+       
+        PreparedStatement stmt = null;
+        List<tabela> resultado = new ArrayList<>();
+        ResultSet rs = null;
+        try {
+            stmt = con.prepareStatement("SELECT * FROM tabela,campeonatos where (tabela.campeonatoid = campeonatos.campeonatoid) and campeonatos.ano = ? ");
+            stmt.setInt(1, 2011);
+             rs = stmt.executeQuery();
+   
+            
+            while (rs.next()) {                
+                
+              tabela t = new tabela();
+              t.setnomes(rs.getString("time"),0);
+              t.setTabela(0, 0, rs.getInt("pontos"));
+              t.setTabela(0, 1, rs.getInt("jogos"));
+              t.setTabela(0, 2, rs.getInt("vitorias"));
+              t.setTabela(0, 3, rs.getInt("derrotas"));
+              t.setTabela(0, 4, rs.getInt("empates"));
+              t.setTabela(0, 5, rs.getInt("gols_feitos"));
+              t.setTabela(0, 6, rs.getInt("gols_sofridos"));
+              t.setTabela(0, 7, rs.getInt("saldo_gols"));        
+              
+              resultado.add(t);
+            }  
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(TabelaDao.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+            
+         ConnectionFactory.closeConnection(con, stmt);
+        
+        }    
+        return resultado;
+    }
+
+public String pesquisaPorAno(int ano){
+        Connection con = ConnectionFactory.getConnection();
+       
+        PreparedStatement stmt = null;
+        List<tabela> resultado = new ArrayList<>();
+        ResultSet rs = null;
+        try {
+            stmt = con.prepareStatement("SELECT * FROM campeonatos where campeonatos.ano = ? ");
+            stmt.setInt(1, ano);
+             rs = stmt.executeQuery();
+   
+            
+            if (rs.next()) {                
+              //passar id do campeonato dentro do insert da tabela
+              int idCampeonato = rs.getInt("campeonatoid");
+              this.campeonatoid = idCampeonato;
+             
+              // insert em tabela 
+              
+              
+            } 
+            else
+            {
+                Connection con1 = ConnectionFactory.getConnection(); 
+                PreparedStatement stmt1 = null;
+                stmt1 = con.prepareStatement("insert into campeonatos(ano) values(?)");
+                stmt1.setInt(1,ano);
+                
+                Connection con2 = ConnectionFactory.getConnection();
+                PreparedStatement stmt2 = null;
+                stmt2 = con2.prepareStatement("SELECT * FROM campeonatos where campeonatos.ano = ? ");
+                stmt2.setInt(1, ano);
+                rs = stmt2.executeQuery();
+                 int idCampeonato = rs.getInt("campeonatoid");
+                 this.campeonatoid = idCampeonato;
+                
+                JOptionPane.showMessageDialog(null, "Salvo com sucesso");
+         } 
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(TabelaDao.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+            
+         ConnectionFactory.closeConnection(con, stmt);
+         ConnectionFactory.closeConnection(con, stmt, rs);
+        
+        }
+        try {
+        
+    } catch (Exception e) {
+    }
+        return null;
+    }
+}
